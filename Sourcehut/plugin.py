runxiyu/limnoria-sourcehut

commit cb0a35dfd076b092dc4d4d45bcc7e5bf4362c055

contrib: fix irc patchset hook when author is not registered
Avoid running into KeyError because the webhook payload does not have
a submitter canonicalName field.

  {
    "data": {
      "webhook": {
        "uuid": "69635b8e-8af5-4292-866b-1e45f75c132a",
        "event": "PATCHSET_RECEIVED",
        "date": "2023-07-11T13:55:17.248351983Z",
        "patchset": {
          "id": 42569,
          "subject": "Commands: add :echo command",
          "version": 1,
          "prefix": "aerc",
          "list": {
            "name": "aerc-devel",
            "owner": {
              "canonicalName": "~rjarry"
            }
          },
          "submitter": {}
        }
      }
    }
  }

Add username and email as fallback values.

Signed-off-by: Robin Jarry <robin@jarry.cc>
Acked-by: Bence Ferdinandy <bence@ferdinandy.com>

Sourcehut/plugin.py
@@ -8,6 +8,7 @@ class Sourcehut(callbacks.Plugin):
     """
     Supybot plugin to receive Sourcehut webhooks
     """
+
     def __init__(self, irc):
         super().__init__(irc)
         httpserver.hook("sourcehut", SourcehutServerCallback(self))
@@ -57,7 +58,13 @@ class SourcehutServerCallback(httpserver.SupyHTTPServerCallback):
                     raise ValueError("unknown list")
                 channel = f"#{patchset['list']['name']}"
                 channel = self.CHANS.get(channel, channel)
-                submitter = patchset["submitter"]["canonicalName"]
+                try:
+                    submitter = patchset["submitter"]["canonicalName"]
+                except KeyError:
+                    try:
+                        submitter = patchset["submitter"]["username"]
+                    except KeyError:
+                        submitter = patchset["submitter"]["email"]
                 msg = f"received {bold(subject)} from {italic(submitter)}: {underline(url)}"
                 self.plugin.announce(channel, msg)
                 handler.send_response(200)

install-webhook.sh
@@ -27,6 +27,8 @@ query {
 				submitter {
 					... on User {
 						canonicalName
+						username
+						email
 					}
 				}
 			}

commit 357bd2c55fe9f4cb7ae353e9fdfb5f13c40d69f6

contrib: actually fix irc patchset hook when author is not registered
When the author is not registered in sr.ht, the schema object is
different.

Fixes: cb0a35dfd076 ("contrib: fix irc patchset hook when author is not registered")
Signed-off-by: Robin Jarry <robin@jarry.cc>

Sourcehut/plugin.py
@@ -62,9 +62,9 @@ class SourcehutServerCallback(httpserver.SupyHTTPServerCallback):
                     submitter = patchset["submitter"]["canonicalName"]
                 except KeyError:
                     try:
-                        submitter = patchset["submitter"]["username"]
+                        submitter = patchset["submitter"]["name"]
                     except KeyError:
-                        submitter = patchset["submitter"]["email"]
+                        submitter = patchset["submitter"]["address"]
                 msg = f"received {bold(subject)} from {italic(submitter)}: {underline(url)}"
                 self.plugin.announce(channel, msg)
                 handler.send_response(200)

install-webhook.sh
@@ -27,8 +27,10 @@ query {
 				submitter {
 					... on User {
 						canonicalName
-						username
-						email
+					}
+					... on Mailbox {
+						name
+						address
 					}
 				}
 			}